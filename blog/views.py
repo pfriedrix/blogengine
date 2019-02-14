924bb2279d6f13866fdef5920848c03ae0182e02
Ivan Romancev <dnl.krsvch@gmail.com>
2019-02-14 10:59:26 +0200

    Initial Commit

---
+++ blog/views.py
@@ -0,0 +1,70 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Tag
+from django.urls import reverse
+from .utils import *
+from django.views.generic import View
+from .forms import TagForm, PostForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+# Create your views here.
+def posts_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts': posts})
+
+
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
+
+
+class PostCreate(LoginRequiredMixin, ObjectCreateMixin, View):
+    form_model = PostForm
+    template = 'blog/post_create_form.html'
+    raise_exception = True
+
+
+class PostUpdate(LoginRequiredMixin, ObjectUpdateMixin, View):
+    model = Post
+    model_form = PostForm
+    template = 'blog/post_update_form.html'
+    raise_exception = True
+
+
+class PostDelete(LoginRequiredMixin, ObjectDeleteMixin, View):
+    model = Post
+    template = 'blog/post_delete_form.html'
+    redirect_url = 'posts_list_url'
+    raise_exception = True
+
+
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
+
+
+class TagCreate(LoginRequiredMixin, ObjectCreateMixin, View):
+    form_model = TagForm
+    template = 'blog/tag_create.html'
+    raise_exception = True
+
+
+class TagUpdate(LoginRequiredMixin, ObjectUpdateMixin, View):
+    model = Tag
+    model_form = TagForm
+    template = 'blog/tag_update_form.html'
+    raise_exception = True
+
+
+class TagDelete(LoginRequiredMixin, ObjectDeleteMixin, View):
+    model = Tag
+    template = 'blog/tag_delete_form.html'
+    redirect_url = 'tags_list_url'
+    raise_exception = True
+
+
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/tags_list.html', context={'tags': tags})
+
+
